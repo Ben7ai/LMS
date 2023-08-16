795f392812d5aa83ee0551bd8a52d76cfa49699b
Benzai <jjag6223@protonmail.com>
2023-08-16 20:58:49 +0800

    PHP Integration V0.2

---
+++ php/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="login">
+        <h1>Login</h1>
+        <form action="authenticate.php" method="post">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <input type="submit" value="Login">
+            <a href="register.php" type="submit" value="Register">Register</a>
+        </form>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</html>
+
---
+++ register.php
@@ -0,0 +1,35 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "lmsdatabase";
+
+
+
+    $conn = mysqli_connect("localhost", "root", "", "lmsdatabase");
+
+    
+        if(mysqli_connect_errno()){
+        exit('Failed to connect to MySQL:' .mysqli_connect_error());
+       }
+
+        if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+        exit('Password must be between 5 and 20 characters long!');
+        }
+
+        if (!filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL)) {
+            exit('Email is not valid!');
+        }
+
+        if($stmt = $conn->prepare('INSERT INTO signup(Email, password) VALUES (?, ?)')){
+             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+             $stmt->bind_param('ss', $_POST['Email'], $password);
+             $stmt->execute();
+        }
+
+        else{
+            echo 'Error';
+        }
+ 
+
+?>
