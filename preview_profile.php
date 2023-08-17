bd6e8f499c255ab46f79dc23def045e28cd9a005
Benzai <jjag6223@protonmail.com>
2023-08-17 11:43:20 +0800

    Comment Integration v1

--- preview_profile.php
+++ preview_profile.php
@@ -1,19 +1,29 @@
 <?php
+// Start the session to access session variables
   session_start();
 
+// Check if the user is not logged in and redirect to index.php
   if (!isset($_SESSION['loggedin'])){
     header('Location: index.php');
     exit;
   }
 
+
+// Database Conenction Information
 $servername = 'localhost';
 $username = 'root';
 $password = '';
 $db_name = 'lmsdatabase';
 
+
+// Establish a connection to the database
 $con = mysqli_connect($servername, $username, $password, $db_name);
+
+// Get user email and ID from session
 $userEmail = $_SESSION['email'];
 $userID = $_SESSION['id'];
+
+// Prepare and execute an SQL SELECT query
 $stmt = $con->prepare('SELECT  first_name, middle_name, last_name, suffix, date_of_birth, current_address, city, province_state, zip_code, second_address, second_city, second_province_state, second_zip_code, elementary_school, high_school FROM user_profile WHERE id =  ?');
 $stmt->bind_param('i', $userID);
 $stmt->execute();
@@ -26,56 +36,64 @@ $stmt->close();
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cyberians LMS: Preview Profile</title>
         <link rel="stylesheet" type="text/css" href="profile-style.css" />
+        
         <!--Bootstrap Library-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
+  </head>
 <body>
 
-    <!--Navigation Bar-->
-  <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="home.html">
-        <img src="./dash-board-pics/logo@2x.png" alt="Logo"  height="30" class="d-inline-block align-text-top">
-        AMA University
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+<!--Navigation Bar-->
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <!-- Navbar Brand and Logo -->
+    <a class="navbar-brand" href="home.html">
+      <img src="./dash-board-pics/logo@2x.png" alt="Logo" height="30" class="d-inline-block align-text-top">
+      AMA University
+    </a>
+
+    <!-- Navbar Toggler Button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Offcanvas Menu -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <!-- Navbar Links and Dropdowns -->
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="courses.html">My Courses</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Student
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="preview_profile.php">Profile</a></li>
+              <li><a class="dropdown-item" href="profile.html">Create Profile</a></li>
+            </ul>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="login.html">Log Out</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="courses.html">My Courses</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Student
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="preview_profile.php">Profile</a></li>
-                <li><a class="dropdown-item" href="profile.html">Create Profile</a></li>
-              </ul>
-              <li class="nav-item">
-                <a class="nav-link" href="login.php">Log Out</a>
-              </li>
-            </li>
-          </ul>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
-  </nav>
+  </div>
+</nav>
+
   
   <!--Title-->
   <h1 class = "text-center">Profile Page</h1>
@@ -83,11 +101,14 @@ $stmt->close();
 
   <!--Content-->
 
+  <!--User Profile-->
   <div class="container-fluid">
     <h3>Account Details</h3>
-    <hr>
-    <h4>User Profile:</h4>
 
+    <hr>
+
+<!-- User Profile Table-->
+    <h4>User Profile:</h4>
     <table class="table table-striped table-hover">
       <tr class="table-secondary">
       <td>
@@ -126,6 +147,7 @@ $stmt->close();
       </tr>
 
       <tr>
+
       <td>
           Email:
         </td>
@@ -146,8 +168,8 @@ $stmt->close();
 
         <hr>
 
+<!-- Current Residence -->
     <h4>Current Residence:</h4>
-
     <table class="table table-striped table-hover">
       <tr class="table-secondary">
       <td>
@@ -186,8 +208,9 @@ $stmt->close();
       </tr>
 
       </table>
-      <h4>Second Residence:</h4>
 
+<!-- Second Residence -->
+      <h4>Second Residence:</h4>
 <table class="table table-striped table-hover">
   <tr class="table-secondary">
   <td>
@@ -223,8 +246,9 @@ $stmt->close();
   </tr>
   </div>
   </table>
+  
+<!-- Education Table -->
   <h4>Education</h4>
-
 <table class="table table-striped table-hover">
   <tr class="table-secondary">
   <td>
@@ -249,6 +273,7 @@ $stmt->close();
   </body>
 </body>
 
+<!--Footer-->
 <footer class="text-body-secondary py-5">
   <div class="container">
     <p class="float-end mb-1">
